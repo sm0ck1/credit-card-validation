cc0fda6d1d070c23a5aedffc86b9db917d226038
serhii.yehorov <serhii.yehorov@p2h.com>
2022-08-22 16:59:37 +0300

    Fix check long american express

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const correctlyExpireDate = require('../helpers/expiry-date')
 const luhnAlgorithm = require('../helpers/luhn')
 
-router.post('/', function (req, res, next) {
+router.post('/', function (req, res) {
     const creditCard = req.body;
     let allowFields = [
         'cardNumber',
@@ -30,12 +30,6 @@ router.post('/', function (req, res, next) {
             validate['expiryDate'] = 'Invalid expiry date. Example: 10/23';
         }
 
-        if (creditCard.cardNumber.length < 16 && creditCard.cardNumber.length > 19) {
-            validate['cardNumber'] = 'Invalid card number. Is between 16 and 19 digits long.';
-        } else if (!luhnAlgorithm(creditCard.cardNumber)) {
-            validate['cardNumber'] = 'Invalid card number. Check card number.';
-        }
-
         let isAmerican = false;
         if (/^(34|37)/.test(creditCard.cardNumber)) {
             isAmerican = true;
@@ -45,6 +39,20 @@ router.post('/', function (req, res, next) {
         } else if (!isAmerican && creditCard.cvv.length !== 3) {
             validate['cvv'] = 'Invalid CVV. Need 3 digits';
         }
+
+        if ((creditCard.cardNumber.length < 16 || creditCard.cardNumber.length > 19) && !isAmerican) {
+            validate['cardNumber'] = 'Invalid card number. Need number between 16 and 19 digits long.';
+        }
+
+        if (creditCard.cardNumber.length !== 15 && isAmerican) {
+            validate['cardNumber'] = 'Invalid card number. Need number 15 digits long.';
+        }
+
+        if (!luhnAlgorithm(creditCard.cardNumber)) {
+            validate['cardNumber'] = 'Invalid card number. Check card number.';
+        }
+
+
     }
 
     res.json({
